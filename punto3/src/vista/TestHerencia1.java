2ccb64ce41894961ee25830ab1e11fe3c218435c
heratok <55002212+heratok@users.noreply.github.com>
2022-05-21 00:40:03 -0500

    Add files via upload

--- punto3/src/vista/TestHerencia1.java
+++ punto3/src/vista/TestHerencia1.java
@@ -5,7 +5,7 @@ import modelo.*;
 public class TestHerencia1 {
 
     public static void main(String[] args) {
-        persona profesor1 = new profesor("Jairo", "Sejuanes", "Prof 34-789-109");
+        persona profesor1 = new profesor("Braulio", "anania", "Prof 33-739-549");
         System.out.print(profesor1.info());
         System.out.println(profesor1);
     }
--- punto3/src/vista/TestHerencia2.java
+++ punto3/src/vista/TestHerencia2.java
@@ -5,7 +5,7 @@ import modelo.*;
 
 public class TestHerencia2 {
     public static void main(String[] args) {
-        profesor profesorInterino = new profesorInterno("Pedro", "Ramiro ", "333-444-212", "Jueves May 12 12:45:02 cet 2020");
+        profesor profesorInterino = new profesorInterno("Camilo", "Torrez ", "321-432-422", "Viernes Maz 11 1:35:32 cet 2022");
         System.out.print(profesorInterino.info());
         System.out.println(profesorInterino);
         
